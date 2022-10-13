afa9fb617408144a02ab5b3eab47fa63c6ba1447
Mary <attamary062@gmail.com>
2022-10-12 23:29:25 -0400

    complete

--- src/org/perscholas/gladiatorarena/Arena.java
+++ src/org/perscholas/gladiatorarena/Arena.java
@@ -1,10 +1,23 @@
 package org.perscholas.gladiatorarena;
 
-public class Arena extends Gladiator{
-    public String name;
+import java.util.ArrayList;
 
-    public Arena (String firstname, String lastname, String weapon, String name) {
-        super(firstname, lastname, weapon);
-        this.name = name;
+public class Arena {
+    public String name;
+    public ArrayList<Gladiator> gladiatorsArray = new ArrayList<Gladiator>();// creating arraylist that will only contain gladiator objects that i created in main.java that i created from the gladitor class
+//the class is not the object the object is define by the class
+    public Arena ( String name) {//the world name in line 10 and line 9name refers to line 7 input but this.name is different because it is refering to the string on line 13
+        this.name = name;//property name of arena class
+        //they all say name but they refers to different pieces
+
     }
+    public void addGladiator (Gladiator gladiator) {
+
+        gladiatorsArray.add(gladiator);
+
+    }
+    public Gladiator pickwinner(){
+        return gladiatorsArray.get(0);
+    }
+
 }
--- src/org/perscholas/gladiatorarena/Main.java
+++ src/org/perscholas/gladiatorarena/Main.java
@@ -3,12 +3,14 @@ package org.perscholas.gladiatorarena;
 public class Main {
     public static void main(String[] args) {
         Gladiator wonder = new Gladiator("wonder", "woman", "spear");
-        Arena accra = new Arena("wonder", "woman", "spear", "Accra");
+        Gladiator flash = new Gladiator("flash", "flash","speed");
+        Arena accra = new Arena( "Accra");
 
-        System.out.println(accra.weapon);
-
+        accra.addGladiator(wonder);
         accra.addGladiator(flash);
-
+        System.out.println(accra.gladiatorsArray);
+        System.out.println(accra.pickwinner());
+        System.out.println(accra.name);//refer to this.name in arena class
 
 
 
